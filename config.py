94ebd27a5341f6c92190c66ddbfb34af24a834b5
Mayberetard <183591460+Mayberetard@users.noreply.github.com>
2025-06-30 09:33:26 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,8 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = "cookie_store"
+OWNER_CONTACT_URL = "https://t.me/YourUsername"
+
+START_IMAGES = [ ]
+
 APPROVED_ADMINS = [1]  # Replace with your Telegram user ID
